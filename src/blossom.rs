5f9155a4c8e7df207a11a30556684dbd700a1241
Elia Perantoni <perantonielia0@gmail.com>
2022-07-27 16:04:21 +0200

    More error context

--- src/blossom.rs
+++ src/blossom.rs
@@ -2,7 +2,7 @@ use prost_reflect::DescriptorPool;
 use prost_reflect::prost_types::FileDescriptorSet;
 use prost_reflect::prost::Message;
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 
 use std::path::Path;
 
@@ -18,9 +18,15 @@ impl Blossom {
     }
 
     pub fn add_descriptor(&mut self, path: &Path) -> Result<()> {
-        let content = std::fs::read(path)?;
-        let file_desc_set = FileDescriptorSet::decode(&content[..])?;
-        self.pool.add_file_descriptor_set(file_desc_set)?;
+        // Read whole file descriptor set to bytes vec
+        let content = std::fs::read(path)
+            .context("reading file descriptor set")?;
+        // Decode it
+        let file_desc_set = FileDescriptorSet::decode(&content[..])
+            .context("decoding file descriptor set")?;
+        // And add it to the pool
+        self.pool.add_file_descriptor_set(file_desc_set)
+            .context("adding file descriptor set to pool")?;
         Ok(())
     }
 }
