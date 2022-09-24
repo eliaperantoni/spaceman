9698cc8fc87f0b5114e151ad98cc6fcad2aadda0
Elia Perantoni <perantonielia0@gmail.com>
2022-09-24 19:17:32 +0200

    Basic backend repo implementation

--- blossom_gui_back/src/main.rs
+++ blossom_gui_back/src/main.rs
@@ -1,10 +1,33 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+use std::{path::Path, sync::RwLock};
+
+use tauri::State;
+
+use blossom_core::Repo;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .manage(RwLock::new(Repo::new()))
+        .invoke_handler(tauri::generate_handler![
+            get_repo_tree,
+            add_protobuf_descriptor,
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn get_repo_tree(repo: State<RwLock<Repo>>) -> Result<Vec<u8>, String> {
+    bincode::serialize(&repo.read().expect("previous holder panicked").view())
+        .map_err(|err| err.to_string())
+}
+
+#[tauri::command]
+fn add_protobuf_descriptor(path: &Path, repo: State<RwLock<Repo>>) -> Result<(), String> {
+    let mut repo = repo.write().expect("previous holder panicked");
+    repo.add_descriptor(path).map_err(|err| err.to_string())
 }
